3b20a4cbd490296275e1859054790786cb0a1fd0
天涯过客 <1157913453@qq.com>
2022-03-12 23:40:10 +0800

    完成overlay挂载镜像层和容器层

--- cmd/init.go
+++ cmd/init.go
@@ -10,6 +10,8 @@ import (
 	"io"
 	"os"
 	"os/exec"
+	"path"
+	"path/filepath"
 	"strings"
 	"syscall"
 )
@@ -42,11 +44,8 @@ var initCmd = &cobra.Command{
 			log.Errorf("获取用户命令错误，命令为空")
 			return
 		}
-		// 在容器内启动并初始化进程
-		defaultMountFlags := syscall.MS_NOEXEC | syscall.MS_NOSUID | syscall.MS_NODEV
 
-		// 使用 mount 先去挂载 proc 文件系统，以便后面通过 ps 等系统命令去查看当前进程资源的情况。
-		syscall.Mount("proc", "/proc", "proc", uintptr(defaultMountFlags), "")
+		setUpMount()
 		path, err := exec.LookPath(cmdArray[0])
 		if err != nil {
 			log.Errorf("Exec loop path error %v", err)
@@ -69,4 +68,69 @@ func readUserCommand() []string {
 	}
 	msgStr := string(msg)
 	return strings.Split(msgStr, " ")
+}
+
+func setUpMount() {
+	pwd, err := os.Getwd()
+	if err != nil {
+		log.Errorf("获取当前目录失败：%v", err)
+		return
+	}
+	log.Infof("当前的目录是：%v", pwd)
+	pivotRoot(pwd)
+
+	// 在容器内启动并初始化进程
+	defaultMountFlags := syscall.MS_NOEXEC | syscall.MS_NOSUID | syscall.MS_NODEV
+	if err = syscall.Mount("proc", "/proc", "proc", uintptr(defaultMountFlags), ""); err != nil {
+		log.Errorf("挂载proc失败：%v", err)
+		return
+	}
+
+	if err = syscall.Mount("tmpfs", "/dev", "tmpfs", syscall.MS_NOSUID|syscall.MS_STRICTATIME, "mode=755"); err != nil {
+		log.Errorf("挂载tmpfs失败：%v", err)
+		return
+	}
+}
+
+func pivotRoot(root string) {
+	/**
+	  为了使当前root的老 root 和新 root 不在同一个文件系统下，我们把root重新mount了一次
+	  bind mount是把相同的内容换了一个挂载点的挂载方法
+	*/
+	if err := syscall.Mount(root, root, "bind", syscall.MS_BIND|syscall.MS_REC, ""); err != nil {
+		log.Errorf("Mount rootfs to itself error: %v", err)
+		return
+	}
+	// 创建 rootfs/.pivot_root存储old_root
+	pivotDir := path.Join(root, ".pivot_root")
+	if err := os.Mkdir(pivotDir, 0777); err != nil {
+		log.Errorf("创建pivotDir失败：%v", err)
+		return
+	}
+
+	// pivot_root 到新的rootfs, 现在老的old_root是挂载在rootfs/.pivot_root
+	// 挂载点现在依然可以在mount命令中看到
+	if err := syscall.PivotRoot(root, pivotDir); err != nil {
+		log.Errorf("挂载就文件系统失败：%v", err)
+		return
+	}
+
+	// 修改当前的工作目录到根目录
+	if err := syscall.Chdir("/"); err != nil {
+		log.Errorf("Chdir错误：%v", err)
+		return
+	}
+
+	pivotDir = filepath.Join("/", ".pivot_root")
+	// umount rootfs/.pivot_root
+	if err := syscall.Unmount(pivotDir, syscall.MNT_DETACH); err != nil {
+		log.Errorf("unmount pivot_root dir %v", err)
+		return
+	}
+	// 删除临时文件夹
+	if err := os.Remove(pivotDir); err != nil {
+		log.Errorf("删除临时文件失败：%v", err)
+		return
+	}
+
 }
--- container/container_process.go
+++ container/container_process.go
@@ -1,8 +1,9 @@
 package container
 
 import (
-	"fmt"
+	"errors"
 	log "github.com/sirupsen/logrus"
+	"io/fs"
 	"os"
 	"os/exec"
 	"syscall"
@@ -25,13 +26,78 @@ func NewParentProcess(tty, interactive bool) (*exec.Cmd, *os.File) {
 
 	// 如果用户指定了-ti参数，就需要把当前进程的输入输出导入到标准输入输出上
 	if tty && interactive {
-		fmt.Println("ti生效了")
 		cmd.Stdin = os.Stdin
 		cmd.Stdout = os.Stdout
 		cmd.Stderr = os.Stderr
 	}
 	cmd.ExtraFiles = []*os.File{readPipe}
+	rootUrl := "/root/"
+	mergeUrl := "/root/merge"
+	NewWorkSpace(rootUrl, mergeUrl)
+	cmd.Dir = mergeUrl
 	return cmd, writePipe
+}
+
+func NewWorkSpace(rootUrl, mergeUrl string) {
+	//CreateLower(rootUrl)
+	CreateUpper(rootUrl)
+	CreateWork(rootUrl)
+	CreateMerge(rootUrl, mergeUrl)
+}
+
+func CreateLower(rootUrl string) {
+	lowerUrl := rootUrl + "lower"
+	if err := os.Mkdir(lowerUrl, 0777); err != nil {
+		if !errors.Is(err, fs.ErrExist) {
+			log.Errorf("创建lowerUrl失败：%v", err)
+		}
+	}
+}
+
+func CreateUpper(rootUrl string) {
+	upperUrl := rootUrl + "upper"
+	if err := os.Mkdir(upperUrl, 0777); err != nil {
+		if !errors.Is(err, fs.ErrExist) {
+			log.Errorf("创建upperUrl失败：%v", err)
+		}
+	}
+}
+
+func CreateWork(rootUrl string) {
+	workUrl := rootUrl + "work"
+	if err := os.Mkdir(workUrl, 0777); err != nil {
+		if !errors.Is(err, fs.ErrExist) {
+			log.Errorf("创建work失败：%v", err)
+		}
+	}
+}
+
+func CreateMerge(rootUrl, mergeUrl string) {
+	if err := os.Mkdir(mergeUrl, 0777); err != nil {
+		if !errors.Is(err, fs.ErrExist) {
+			log.Errorf("创建mergeUrl失败：%v", err)
+			return
+		}
+	}
+
+	dirs := "lowerdir=" + rootUrl + "busybox," + "upperdir=" + rootUrl + "upper," + "workdir=" + rootUrl + "work"
+	cmd := exec.Command("mount", "-t", "overlay", "overlay", "-o", dirs, mergeUrl)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	if err := cmd.Run(); err != nil {
+		log.Errorf("cmd.Run()错误：%v", err)
+	}
+}
+
+func PathExits(path string) (bool, error) {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true, nil
+	}
+	if errors.Is(err, fs.ErrNotExist) {
+		return false, nil
+	}
+	return false, err
 }
 
 func NewPipe() (*os.File, *os.File, error) {
@@ -40,4 +106,30 @@ func NewPipe() (*os.File, *os.File, error) {
 		return nil, nil, err
 	}
 	return read, write, nil
+}
+
+func DeleteWorkSpace(rootUrl, mntUrl string) {
+	DeleteMountPoint(mntUrl)
+	DeleteWriteLayer(rootUrl)
+}
+
+func DeleteMountPoint(mntUrl string) {
+	cmd := exec.Command("umount", mntUrl)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	if err := cmd.Run(); err != nil {
+		log.Errorf("卸载%s失败：%v", mntUrl, err)
+	}
+
+	if err := os.RemoveAll(mntUrl); err != nil {
+		log.Errorf("删除%s失败：%v", mntUrl, err)
+	}
+}
+
+func DeleteWriteLayer(rootUrl string) {
+	writeLayerUrl := rootUrl + "writeLayer"
+	if err := os.RemoveAll(writeLayerUrl); err != nil {
+		log.Errorf("删除读写层失败：%v", err)
+	}
+
 }
